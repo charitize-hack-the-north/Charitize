f8d76924500ee40c10546fc360245a35835d92c7
Winston.Lu <wlu2268@gmail.com>
2022-09-17 23:04:09 -0400

    deleted changes

--- src/components/dashboard/CharityCard.js
+++ src/components/dashboard/CharityCard.js
@@ -3,12 +3,7 @@ import React, { useState } from "react";
 function CharityCard(props) {
   return (
     <div>
-        <div className="bg-white shadow-lg rounded-lg m-8 p-8 flex md:bg-orange w-60 h-72">
-            <div class="px-3 py-1 content-center mx-auto justify-center w-60">
-                <img src={require('./../../img/charity-1.png')} className="pl-4"/>
-                <h1 className="font-medium text-xl w-64 pr-28 text-lg">Canadian Cancer Society</h1>
-            </div>
-        </div> 
+        
     </div>
   );
 }
